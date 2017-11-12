44dab89db6dcf62799c2381436ae4dd26ad57a88
Baptiste Langlade <baptouuuu@gmail.com>
2017-11-12 16:29:55 +0100

    add message model

---
+++ src/Message.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\Git;
+
+use Innmind\Git\Exception\DomainException;
+use Innmind\Immutable\Str;
+
+final class Message
+{
+    private $value;
+
+    public function __construct(string $message)
+    {
+        if ((new Str($message))->trim()->length() === 0) {
+            throw new DomainException;
+        }
+
+        $this->value = $message;
+    }
+
+    public function __toString(): string
+    {
+        return $this->value;
+    }
+}
